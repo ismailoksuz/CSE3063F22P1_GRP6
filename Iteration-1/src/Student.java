900a0183a3cd606518e713a1224d2250f3927a69
serkankorkut17 <srknkrktgs@gmail.com>
2022-11-10 15:35:02 +0300

    Adding Student class - needs update

---
+++ Iteration-1/src/Student.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Student {
+    // private StudentID studentID;
+    // private Transcript transcript;
+    // private Advisor advisor;
+    // private ArrayList<Course> requestedCourses;
+    // private ArrayList<Course> takenCourses;
+    private int registrationYear;
+    private int currentYear;
+    private int semester;
+
+    public Student() {
+    }
+
+    public void addCourse() {
+    }
+
+    public void dropCourse() {
+    }
+
+    public void sendRequest() {
+    }
+}
