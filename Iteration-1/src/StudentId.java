49f5548b48ea8cdde6a1a237b135f22bbe334aca
erdemphl <85944761+erdemphl@users.noreply.github.com>
2022-11-10 17:32:34 +0300

    Add files via upload

---
+++ Iteration-1/src/StudentId.java
@@ -0,0 +1,40 @@
+
+public class StudentId {
+	private int cseCode;
+	private int year;
+	private int order;
+	
+	public StudentId(int cseCode, int year, int order) {
+		this.cseCode = cseCode;
+		this.year = year;
+		this.order = order;
+	}
+	
+	public String createStudentId() {
+		return Integer.toString(this.cseCode) + Integer.toString(this.year) + Integer.toString(this.order);
+	}
+
+	public int getCseCode() {
+		return cseCode;
+	}
+
+	public void setCseCode(int cseCode) {
+		this.cseCode = cseCode;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
+	}
+}
