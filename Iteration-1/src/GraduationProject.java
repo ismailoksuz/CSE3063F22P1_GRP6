cb0a0646bb1213c170570712db2bd9b9cecee66f
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-11-10 19:13:06 +0300

    Update GraduationProject.java

--- Iteration-1/src/GraduationProject.java
+++ Iteration-1/src/GraduationProject.java
@@ -1,24 +1,8 @@
 public class GraduationProject extends MandatoryCourse {
-    private String courseCode;  
-    private String courseName;
     public GraduationProject(){
-
     }
     public boolean isEligibleToRequest(Transcript transcript){
         transcript.creditCompleted=creditCompleted;
         return (creditCompleted>=160) ? true : false;
-    }
-    public void setCourseCode(String courseCode){
-        this.courseCode=courseCode;
-    }
-    public void setCourseName(String courseName){
-        this.courseName=courseName;
-    }
-    public String getCourseCode(){
-        return this.courseCode;
-    }
-    public String getCourseName(){
-        return this.courseName;        
-    }   
-    
+    } 
 }
