eaf8ebb443099024d54658dbc8f89826627a2064
Mustafa Yanar <mustafayanar0007@gmail.com>
2022-11-11 21:11:10 +0300

    Instructor and Advisor added

--- Iteration-1/src/Advisor.java
+++ Iteration-1/src/Advisor.java
@@ -1,14 +1,43 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class Advisor {
+public class Advisor extends Instructor {
     private List<Student> students;
 
-    public Advisor() {
+    public Advisor(String firstName, String lastName) {
+        super(firstName, lastName);
+        this.students = new ArrayList<Student>();
+        System.out.println("Advisor created");
     }
-    public Advisor(List<Student> students) {
+
+    public Advisor(String firstName, String lastName, List<Course> givenCourses) {
+        super(firstName, lastName, givenCourses);
+        this.students = new ArrayList<Student>();
+        System.out.println("Advisor created");
+    }
+
+    public List<Student> getStudents() {
+        return this.students;
+    }
+
+    public void setStudents(List<Student> students) {
         this.students = students;
+        System.out.println("Advisor students set");
     }
-    public void approveRequest(Student student) {
-        System.out.println("Request approved");
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+        System.out.println("Advisor student added");
+    }
+
+    public void removeStudent(Student student) {
+        this.students.remove(student);
+        System.out.println("Advisor student removed");
+    }
+
+    public void adviseStudent(Student student) {
+        this.addStudent(student);
+        //student.setAdvisor(this);
+        System.out.println("Advisor advised student");
     }
 }
