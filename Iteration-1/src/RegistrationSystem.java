239ad9d4a8aabe9cf7fed82d337eb7297ccb616a
onuralkurt <54157952+onuralkurt@users.noreply.github.com>
2022-11-12 17:37:04 +0300

    Update RegistrationSystem.java

--- Iteration-1/src/RegistrationSystem.java
+++ Iteration-1/src/RegistrationSystem.java
@@ -1,45 +1,211 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class RegistrationSystem {
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+public class Registration {
 
-    private ArrayList<Student> studentsList;
-    private ArrayList<Course> coursesList;
-    private ArrayList<Advisor> advisorsList;
+    private ArrayList<Course> coursesList = new ArrayList<>();
+    private ArrayList<MandatoryCourse> mandotoryCourses = new ArrayList<>();
+    private ArrayList<GraduationProject> graduationCourses = new ArrayList<>();
+    private ArrayList<TechnicalElective> technicalElectives = new ArrayList<>();
+    private ArrayList<FacultyTechnicalElective> facultyTechnicalElectives = new ArrayList<>();
+    private ArrayList<NonTechnicalElective> nonTechnicalElectives = new ArrayList<>();
 
-    public RegistrationSystem() throws Exception {
-        Parser parser = new Parser();
-        studentsList = parser.parseStudents();
-        coursesList = parser.parseCourses();
-        advisorsList = parser.parseAdvisors();
+    private Course courseIsThereOrNot(String courseCode){
+        for (Course c: coursesList){
+            if (c.getCourseCode().equals(courseCode)){
+                return c;
+            }
+        }
+        return null;
     }
 
-    public void failRandomCourses() {
+    private void readMandotory(JSONObject input){
+            JSONArray inputCourses = (JSONArray) input.get("MandatoryCourses");
+            for (Object c : inputCourses) {
+                JSONObject course = (JSONObject) c;
+                String courseName = (String) course.get("courseName");
+                String courseCode = (String) course.get("courseCode");
+                int courseQuato= (int) (long) course.get("quota");
+                int courseSemester = (int) (long) course.get("semester");
+                int courseCredit = (int) (long) course.get("credits");
+                int courseDay = (int) (long) course.get("courseDay");
+                String courseHour = (String) course.get("courseHour");
+
+                ArrayList<Course> prequisitesCourse = new ArrayList<>();
+                JSONArray prequisites = (JSONArray) course.get("preRequisites");
+                for (Object p: prequisites){
+                    prequisitesCourse.add(courseIsThereOrNot((String) p));
+                }
+                MandatoryCourse mandatoryCourse = new MandatoryCourse(courseName, courseCode, courseCredit, courseDay,
+                        courseHour, courseSemester, courseQuato,prequisitesCourse);
+
+                coursesList.add(mandatoryCourse);
+                mandotoryCourses.add(mandatoryCourse);
+
+            }
+
     }
 
-    public void storeStudents() {
+    private void readNTE(JSONObject input){
+        JSONArray inputNTECourses = (JSONArray) input.get("nonTechnicalElectiveCourses");
+        for (Object c : inputNTECourses){
+            JSONObject course = (JSONObject) c;
+            String courseName = (String) course.get("courseName");
+            String courseCode = (String) course.get("courseCode");
+            int courseQuato= (int) (long) course.get("quota");
+            int courseCredit = (int) (long) course.get("credits");
+            int courseDay = (int) (long) course.get("courseDay");
+            String courseHour = (String) course.get("coursHour");
+            JSONArray semesters = (JSONArray) course.get("semester");
+
+            NonTechnicalElective nonTechnicalElective = new NonTechnicalElective(courseName, courseCode, courseCredit,
+                    courseDay, courseHour, courseQuato, semesters);
+            nonTechnicalElectives.add(nonTechnicalElective);
+            coursesList.add(nonTechnicalElective);
+
+        }
     }
 
-    public ArrayList<Student> getStudentsList() {
-        return this.studentsList;
+    private void readTE(JSONObject input) {
+        JSONArray inputNTECourses = (JSONArray) input.get("technicalElectiveCourses");
+        for (Object c : inputNTECourses) {
+            JSONObject course = (JSONObject) c;
+            String courseName = (String) course.get("courseName");
+            String courseCode = (String) course.get("courseCode");
+            int courseQuato = (int) (long) course.get("quota");
+            int courseCredit = (int) (long) course.get("credits");
+            int courseDay = (int) (long) course.get("courseDay");
+            String courseHour = (String) course.get("courseHour");
+            int requiredCredit = (int) (long) course.get("requiredCredits");
+
+
+            ArrayList<Course> prequisitesCourse = new ArrayList<>();
+            JSONArray prequisites = (JSONArray) course.get("preRequisites");
+            for (Object p : prequisites) {
+                prequisitesCourse.add(courseIsThereOrNot((String) p));
+            }
+            ;
+
+            JSONArray semesters = (JSONArray) course.get("semester");
+            TechnicalElective techElectiveCourse = new TechnicalElective(courseName, courseCode, courseCredit, courseDay,
+                    courseHour, courseQuato, semesters, requiredCredit, prequisites);
+
+            coursesList.add(techElectiveCourse);
+            technicalElectives.add(techElectiveCourse);
+
+
+        }
     }
 
-    public void setStudentsList(ArrayList<Student> studentsList) {
-        this.studentsList = studentsList;
+
+
+    private void readFTE(JSONObject input){
+        JSONArray inputNTECourses = (JSONArray) input.get("facultyTechnicalElectiveCourses");
+        for (Object c : inputNTECourses){
+            JSONObject course = (JSONObject) c;
+            String courseName = (String) course.get("courseName");
+            String courseCode = (String) course.get("courseCode");
+            int courseQuato= (int) (long) course.get("quota");
+            int courseCredit = (int) (long) course.get("credits");
+            int courseDay = (int) (long) course.get("courseDay");
+            String courseHour = (String) course.get("courseHour");
+
+            JSONArray semesters = (JSONArray) course.get("semester");
+            ArrayList<Course> prequisitesCourse = new ArrayList<>();
+            JSONArray prequisites = (JSONArray) course.get("preRequisites");
+            for (Object p: prequisites){
+                prequisitesCourse.add(courseIsThereOrNot((String) p));
+            };
+
+            FacultyTechnicalElective facultyTechnicalElective = new FacultyTechnicalElective(courseName, courseCode,
+                    courseCredit, courseDay, courseHour, courseQuato, semesters, prequisitesCourse);
+            facultyTechnicalElectives.add(facultyTechnicalElective);
+            coursesList.add(facultyTechnicalElective);
+
+
+        }
     }
 
-    public ArrayList<Course> getCoursesList() {
-        return this.coursesList;
+    private void readGraduationProject(JSONObject input){
+        JSONArray inputNTECourses = (JSONArray) input.get("graduationProject");
+        for (Object c : inputNTECourses){
+            JSONObject course = (JSONObject) c;
+            String courseName = (String) course.get("courseName");
+            String courseCode = (String) course.get("courseCode");
+            int courseQuato= (int) (long) course.get("quota");
+            int courseSemester = (int) (long) course.get("semester");
+            int courseCredit = (int) (long) course.get("credits");
+            int courseDay = (int) (long) course.get("courseDay");
+            int requiredCredit = (int) (long) course.get("requiredCredits");
+            String courseHour = (String) course.get("courseHour");
+
+            ArrayList<Course> prequisitesCourse = new ArrayList<>();
+            JSONArray prequisites = (JSONArray) course.get("preRequisites");
+            for (Object p: prequisites){
+                prequisitesCourse.add(courseIsThereOrNot((String) p));
+            };
+            GraduationProject graduationProject = new GraduationProject(courseName, courseCode, courseCredit, courseDay,
+                    courseHour, courseQuato, courseSemester, prequisites, requiredCredit);
+
+            coursesList.add(graduationProject);
+            graduationCourses.add(graduationProject);
+
+
+        }
     }
 
-    public void setCoursesList(ArrayList<Course> coursesList) {
-        this.coursesList = coursesList;
+    private void readInput(){
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject input = (JSONObject) parser.parse(new FileReader("input.json"));
+
+            readMandotory(input);
+            readGraduationProject(input);
+            readTE(input);
+            readFTE(input);
+        } catch (IOException | ParseException e){
+            e.printStackTrace();
+        }
     }
 
-    public ArrayList<Advisor> getAdvisorsList() {
-        return this.advisorsList;
+    public void startSimulation(){
+        readInput();
+        for (Course o: mandotoryCourses){
+            System.out.println(o.getCourseName().toString());
+            //System.out.println(o.getCourseHour().toString());
+
+        }
+
+        for (GraduationProject c: graduationCourses){
+            System.out.println(c.getCourseName());
+
+
+        }
+
+        for (TechnicalElective t: technicalElectives){
+            System.out.println(t.getCourseName());
+            System.out.println(t.getSemester());
+        }
+
+        for (FacultyTechnicalElective f: facultyTechnicalElectives){
+            System.out.println(f.getCourseName());
+            //System.out.println(f.getSemester());
+        }
+
+        for (NonTechnicalElective n: nonTechnicalElectives){
+            System.out.println(n.getCourseName());
+        }
+
+
     }
 
-    public void setAdvisorsList(ArrayList<Advisor> advisorsList) {
-        this.advisorsList = advisorsList;
+
+
+
     }
-}
+
