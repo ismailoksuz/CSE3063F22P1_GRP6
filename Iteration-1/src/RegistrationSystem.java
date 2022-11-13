8fa78fde5921ab51863d75be15a92933aa799dc3
onuralkurt <54157952+onuralkurt@users.noreply.github.com>
2022-11-13 04:21:25 +0300

    Update RegistrationSystem.java

--- Iteration-1/src/RegistrationSystem.java
+++ Iteration-1/src/RegistrationSystem.java
@@ -1,6 +1,11 @@
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -8,6 +13,8 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 public class Registration {
 
+    private ArrayList<Advisor> advisorList = new ArrayList<>();
+    private ArrayList<Student> studentList = new ArrayList<>();
     private ArrayList<Course> coursesList = new ArrayList<>();
     private ArrayList<MandatoryCourse> mandotoryCourses = new ArrayList<>();
     private ArrayList<GraduationProject> graduationCourses = new ArrayList<>();
@@ -159,6 +166,81 @@ public class Registration {
         }
     }
 
+    private void readAdvisorInput(JSONObject advisor){
+        JSONArray inputAdvisors = (JSONArray) advisor.get("advisors");
+        for (Object a : inputAdvisors){
+            JSONObject advisors = (JSONObject) a;
+            String firstName = (String) advisors.get("firstName");
+            String lastName = (String) advisors.get("lastName");
+            Advisor newAdvisor = new Advisor(firstName, lastName);
+            advisorList.add(newAdvisor);
+
+        }
+    }
+    private void assignAdvisor(ArrayList<Student> studentList){
+        ArrayList<Student> copyStudentList = new ArrayList<>();
+        for (Student a : studentList){
+            copyStudentList.add(a);
+        }
+        if (copyStudentList.size() > 0){
+
+            int count = 0;
+
+            while (advisorList.size() > count) {
+                if (copyStudentList.size() == 0) {
+                    break;
+                } else {
+                    System.out.println(copyStudentList.size());
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, copyStudentList.size());
+                    advisorList.get(count).addStudent(copyStudentList.get(randomNum));
+                    copyStudentList.get(randomNum).setAdvisor(advisorList.get(count));
+                    copyStudentList.remove(randomNum);
+                    count++;
+                    if (count == advisorList.size()) {
+                        count = 0;
+                    }
+                }
+            }
+
+
+        }
+    }
+
+    private void readStudentInput(JSONObject student){
+        JSONArray inputStudents = (JSONArray) student.get("students");
+        for (Object s : inputStudents){
+            JSONObject students = (JSONObject) s;
+            String studentFirstName = (String) students.get("firstName");
+            String studentLastName = (String) students.get("lastName");
+            int studentRegistirationYear = (int) (long) students.get("registrationYear");
+            int studentOrder = (int)(long) students.get("order");
+            int currentYear = Year.now().getValue();
+            String studentName = (studentFirstName + " " + studentLastName);
+            Student newStudent = new Student(studentName, studentRegistirationYear, studentOrder, currentYear);
+            studentList.add(newStudent);
+            newStudent.setSemester(calculateSemester(newStudent));
+
+
+        }
+
+    }
+    private int calculateSemester(Student student){
+        ArrayList<String> monthListFirstSemester = new ArrayList<>(Arrays.asList("JANUARY", "FEBRUARY", "SEPTEMBER",
+                "OCTOBER", "NOVEMBER", "DECEMBER"));
+        ArrayList<String> monthListSecondSemester = new ArrayList<>(Arrays.asList("MARCH", "APRIL", "MAY", "JUNE",
+                "JULLY", "AUGUST"));
+        LocalDate currentdate = LocalDate.now();
+        Month currentMonth = currentdate.getMonth();
+        String thisMonth = currentMonth.toString();
+        int calculatedSemester = 0;
+        if (monthListFirstSemester.contains(thisMonth)){
+            calculatedSemester = (2 * (student.getCurrentYear() - student.getRegistrationYear())+1);
+        } else if(monthListSecondSemester.contains((thisMonth))){
+            calculatedSemester = (2 * (student.getCurrentYear() - student.getRegistrationYear()));
+        }
+        return calculatedSemester;
+    }
+
     private void readInput(){
         try {
             JSONParser parser = new JSONParser();
@@ -172,9 +254,33 @@ public class Registration {
             e.printStackTrace();
         }
     }
+    private void readStudent(){
+        try{
+            JSONParser parser = new JSONParser();
+            JSONObject student = (JSONObject) parser.parse(new FileReader("students.json"));
+            readStudentInput(student);
+        } catch (IOException | ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void readAdvisors(){
+        try{
+            JSONParser parser = new JSONParser();
+            JSONObject advisor = (JSONObject) parser.parse(new FileReader("advisor.json"));
+            readAdvisorInput(advisor);
+        } catch (IOException | ParseException e){
+            e.printStackTrace();
+        }
+    }
 
     public void startSimulation(){
+        readAdvisors();
         readInput();
+        readStudent();
+        assignAdvisor(studentList);
+
+        /*
         for (Course o: mandotoryCourses){
             System.out.println(o.getCourseName().toString());
             //System.out.println(o.getCourseHour().toString());
@@ -201,6 +307,28 @@ public class Registration {
             System.out.println(n.getCourseName());
         }
 
+        for(Student s : studentList){
+            System.out.printf("%s , %d yılında üniversiteye girdi, %d. Semester", s.getStudentName(),
+                    s.getRegistrationYear(), s.getSemester());
+            System.out.println();
+        }
+
+
+
+        for (Advisor a : advisorList){
+            System.out.println(a.getFirstName());
+            System.out.println(a.getEmail());
+            System.out.println(a.getStudents());
+
+        }
+
+        System.out.println(studentList.size());
+        System.out.println(advisorList.size());
+        */
+        for(Student s : studentList){
+            System.out.printf("%s , Danışman: %s", s.getStudentName(),s.getAdvisor().getEmail());
+            System.out.println();
+        }
 
     }
 
