03998273fed1d06635da2478cde05f398278e62e
emresagiroglu <94112475+emresagiroglu@users.noreply.github.com>
2022-11-15 01:34:57 +0300

    Update NonTechnicalElective.java

--- Iteration-1/src/NonTechnicalElective.java
+++ Iteration-1/src/NonTechnicalElective.java
@@ -2,15 +2,13 @@ import java.util.ArrayList;
 
 public class NonTechnicalElective extends ElectiveCourse {
 
-
-    public NonTechnicalElective(){}
-    public NonTechnicalElective (String  courseName, String courseCode, int courseCredit, int courseDay,
-                                 String courseHour, int quato, ArrayList<Integer> semesters){
+    public NonTechnicalElective(String courseName, String courseCode, int courseCredit, int courseDay,
+            String courseHour, int quato, ArrayList<Integer> semesters) {
         super(courseName, courseCode, courseCredit, courseDay, courseHour, quato, semesters);
 
     }
 
-    public boolean isEligibleToRequest(Student student){
+    public boolean isEligibleToRequest(Student student) {
         return semesterControl(student);
     }
 
