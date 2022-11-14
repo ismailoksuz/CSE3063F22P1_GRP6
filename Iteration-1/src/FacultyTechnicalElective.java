b1c5806038212586d2045038d5fbbe8313b353f7
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-11-15 01:41:00 +0300

    Update FacultyTechnicalElective.java

--- Iteration-1/src/FacultyTechnicalElective.java
+++ Iteration-1/src/FacultyTechnicalElective.java
@@ -3,19 +3,17 @@ import java.util.ArrayList;
 public class FacultyTechnicalElective extends ElectiveCourse {
         private ArrayList<Course> prequisites;
 
-        public FacultyTechnicalElective(){};
-        public FacultyTechnicalElective(String  courseName, String courseCode, int courseCredit, int courseDay,
-                                        String courseHour, int quato, ArrayList<Integer> semesters,
-                                        ArrayList<Course> prequisites){
+        public FacultyTechnicalElective(String courseName, String courseCode, int courseCredit, int courseDay,
+                        String courseHour, int quato, ArrayList<Integer> semesters,
+                        ArrayList<Course> prequisites) {
                 super(courseName, courseCode, courseCredit, courseDay, courseHour, quato, semesters);
                 this.prequisites = prequisites;
         }
 
-        public boolean isEligibleToRequest(Student student){
+        @Override
+        public boolean isEligibleToRequest(Student student) {
                 return semesterControl(student) && student.getTranscript().hasBeenPassedCourses(this.getPrequisites());
         }
-
-
 
         //GETTER & SETTER
 
