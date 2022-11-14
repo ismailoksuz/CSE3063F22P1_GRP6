b3c431db4a57d765bc223070213ba302b7c1a766
Serkan Korkut <83539054+serkankorkut17@users.noreply.github.com>
2022-11-14 23:43:32 +0100

    Update Schedule.java

--- Iteration-1/src/Schedule.java
+++ Iteration-1/src/Schedule.java
@@ -2,6 +2,10 @@ public class Schedule {
     private int courseDay;
     private String courseHour;
 
+    public enum Day {
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
     public Schedule(int courseDay, String courseHour) {
         this.courseDay = courseDay;
         this.courseHour = courseHour;
