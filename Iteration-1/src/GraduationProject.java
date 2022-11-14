0a5814346258836d125bbeb868049ee90928affa
erdemphl <85944761+erdemphl@users.noreply.github.com>
2022-11-15 01:31:37 +0300

    Update GraduationProject.java

--- Iteration-1/src/GraduationProject.java
+++ Iteration-1/src/GraduationProject.java
@@ -4,28 +4,31 @@ public class GraduationProject extends MandatoryCourse {
 
     private int requiredCredits;
 
-    public GraduationProject(){
-    }
-
-
-
     public GraduationProject(String courseName, String courseCode, int courseCredit, int courseDay, String courseHour,
-                             int quato, int semester, ArrayList<Course> prequisites, int requiredCredits){
+            int quato, int semester, ArrayList<Course> prequisites, int requiredCredits) {
         super(courseName, courseCode, courseCredit, courseDay, courseHour, quato, semester, prequisites);
         this.requiredCredits = requiredCredits;
     }
 
     @Override
-    public boolean isEligibleToRequest(Student student){
+    public boolean isEligibleToRequest(Student student) {
 
         return super.isEligibleToRequest(student) && checkRequiredCredit(student);
 
     }
 
-    public boolean checkRequiredCredit(Student student){
-        return student.getTranscript().getCreditCompleted() >= requiredCredits;
-    }
+    public boolean checkRequiredCredit(Student student) {
+        if (student.getTranscript().getCreditCompleted() >= requiredCredits) {
+            return true;
+        } else {
+            this.setFailedCredits(getFailedCredits() + 1);
+            this.getStudentsFailedCredits().add(student);
+            student.getStudentOutput().add("The advisor didn't approve graduation project " + this.getCourseCode() +
+                    " because student completed credits < " + this.getRequiredCredits());
+            return false;
+        }
 
+    }
 
     // GETTER & SETTER
     public int getRequiredCredits() {
