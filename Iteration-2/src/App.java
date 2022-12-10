e7e2f400e84541d17e877be112e39d7e2bc54368
emresagiroglu <94112475+emresagiroglu@users.noreply.github.com>
2022-12-10 14:57:09 +0300

    Update App.java
    Log4j

--- Iteration-2/src/App.java
+++ Iteration-2/src/App.java
@@ -1,8 +1,37 @@
+import java.util.Scanner;
+import org.apache.log4j.Logger;
+
 public class App {
+    static Logger log = Logger.getLogger(App.class);
+
+    public static void main(String[] args) throws Exception {
+        RegistrationSystem rs = new RegistrationSystem();
+        log.info("Registration for " + rs.getCurrentSemester() + " semester successfully completed.");
+
+        if (rs.getCurrentSemester().equals("spring"))
+            System.exit(0);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Do you want to simulate for spring semester with same transcripts? Y/N");
+        char userChoice = input.next().charAt(0);
+
+        if (Character.toLowerCase(userChoice) == 'y') {
+            rs.simulateSpringAfterFall();
+            log.info("Registration for " + rs.getCurrentSemester() + " successfully completed.");
+        }
+        log.info("------END--------");
+        input.close();
+    }
+}
+
+
+
+
+/*public class App {
     public static void main(String[] args) throws Exception {
         RegistrationSystem rs = new RegistrationSystem();
         /*  Path currentRelativePath = Paths.get("");
         String s = currentRelativePath.toAbsolutePath().toString();
         System.out.println("Current absolute path is: " + s); */
     }
-}
+}*/
