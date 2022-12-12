59395b68d7c658d3c678450804aa60e92a862178
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-12-12 18:37:44 +0300

    Update ElectiveCourse.java

--- Iteration-2/src/ElectiveCourse.java
+++ Iteration-2/src/ElectiveCourse.java
@@ -1,13 +1,15 @@
 import java.util.ArrayList;
+import org.apache.log4j.Logger;
 
 public abstract class ElectiveCourse extends Course {
-
+    static Logger log = Logger.getLogger(ElectiveCourse.class);
     private ArrayList<Integer> semesters;
 
     public ElectiveCourse(String courseName, String courseCode, int courseCredit, int courseDay, String courseHour,
             int quato, ArrayList<Integer> semesters) {
         super(courseName, courseCode, courseCredit, courseDay, courseHour, quato);
         this.semesters = semesters;
+        log.info(this.getCourseName() + " (" + this.getCourseCode() + ")" + " named elective course created.");
     }
 
     public boolean semesterControl(Student student) {
@@ -15,9 +17,13 @@ public abstract class ElectiveCourse extends Course {
         for (int semester : this.getSemesters()) {
             if (student.getSemester() == semester) {
                 isTrue = true;
+                /* log.info(this.getCourseName() + ": Student " + student.getStudentName()
+                        + " can take the course this semester."); */
                 break;
             }
         }
+        /* log.info(this.getCourseName() + ": Student " + student.getStudentName()
+                + " can not take the course this semester."); */
         return isTrue;
     }
 
@@ -26,9 +32,13 @@ public abstract class ElectiveCourse extends Course {
         for (Integer s : this.getSemesters()) {
             if (s.intValue() == semester) {
                 isTrue = true;
+                /* log.info(this.getCourseName() + ": The course can be taken in the given semester" + "(" + semester + ")"
+                        + "."); */
                 break;
             }
         }
+        /* log.info(this.getCourseName() + ": The course can not be taken in the given semester" + "(" + semester + ")"
+                + "."); */
         return isTrue;
     }
 
@@ -38,5 +48,6 @@ public abstract class ElectiveCourse extends Course {
 
     public void setSemesters(ArrayList<Integer> semesters) {
         this.semesters = semesters;
+        /* log.info(this.getCourseName() + ": Course semesters changed."); */
     }
 }
