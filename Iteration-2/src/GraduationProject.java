946072bf4fcc811587e487ee2fb28200222fb4d8
Erdem Pehlivanlar <85944761+erdemphl@users.noreply.github.com>
2022-12-12 19:05:17 +0300

    Update GraduationProject.java

--- Iteration-2/src/GraduationProject.java
+++ Iteration-2/src/GraduationProject.java
@@ -1,33 +1,46 @@
 import java.util.ArrayList;
+import org.apache.log4j.Logger;
 
 public class GraduationProject extends MandatoryCourse implements ICreditRequirement {
-
+    static Logger log = Logger.getLogger(GraduationProject.class);
     private int requiredCredits;
 
     public GraduationProject(String courseName, String courseCode, int courseCredit, int courseDay, String courseHour,
             int quato, int semester, ArrayList<Course> prequisites, int requiredCredits) {
         super(courseName, courseCode, courseCredit, courseDay, courseHour, quato, semester, prequisites);
         this.requiredCredits = requiredCredits;
+        log.info(this.getCourseName() + " (" + this.getCourseCode() + ")"
+                + " named graduation project created.");
     }
 
     @Override
     public boolean isEligibleToRequest(Student student) {
-
+        /* log.info(student.getStudentName() + " can "
+                + ((super.isEligibleToRequest(student) && checkRequiredCredit(student))
+                        ? ""
+                        : "not")
+                + "enroll this course: "
+                + this.getCourseName()); */
         return super.isEligibleToRequest(student) && checkRequiredCredit(student);
-
     }
 
     @Override
     public boolean checkRequiredCredit(Student student) {
         if (student.getTranscript().getCreditCompleted() >= requiredCredits) {
+            /* log.info(
+                    "Student has enough credits. " + student.getStudentName() + " can enroll " + this.getCourseName()
+                            + "."); */
             return true;
         } else {
             this.setFailedCredits(getFailedCredits() + 1);
             student.getStudentOutput().add("The advisor didn't approve graduation project " + this.getCourseCode() +
                     " because student completed credits < " + this.getRequiredCredits());
+            log.info(
+                    "Student has not enough credits. " + student.getStudentName() + " cannot enroll "
+                            + this.getCourseName()
+                            + ".");
             return false;
         }
-
     }
 
     // GETTER & SETTER
@@ -37,5 +50,7 @@ public class GraduationProject extends MandatoryCourse implements ICreditRequire
 
     public void setRequiredCredits(int requiredCredits) {
         this.requiredCredits = requiredCredits;
+        /* log.info(this.getCourseName() + ": Course required credits changed." + "(" + "New: " + this.getRequiredCredits()
+                + ")"); */
     }
 }
