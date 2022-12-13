37779dc21b314dbf35529b45ac556961728681fb
Mustafa Yanar <mustafayanar0007@gmail.com>
2022-12-13 22:04:01 +0300

    Schedule Unit Test has been added

---
+++ Iteration-2/src/ScheduleTest.java
@@ -0,0 +1,13 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ScheduleTest {
+
+    Schedule schedule = new Schedule(1, "08:00-09:00");
+    @Test
+    public void testIsCollision() {
+        assertEquals(schedule.isCollision(new Schedule(1, "08:00-09:00")), true);
+    }
+}
