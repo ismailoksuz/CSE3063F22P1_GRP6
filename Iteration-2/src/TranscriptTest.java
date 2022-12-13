fd05207e9304608ce3c04576e38ec3a3591190ac
Mustafa Yanar <mustafayanar0007@gmail.com>
2022-12-13 21:04:19 +0300

    Unit tests have been updated

--- Iteration-2/src/TranscriptTest.java
+++ Iteration-2/src/TranscriptTest.java
@@ -19,27 +19,16 @@ public class TranscriptTest {
     @Test
     public void testCalculateGpa() {
         HashMap<Course,Grade> mandatoryCourses = new HashMap<Course,Grade>();
-        mandatoryCourses.put(new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null), new Grade("AA"));
-        mandatoryCourses.put(new MandatoryCourse("English", "ENG101", 3, 1, "08:00-09:00", 30, 1, null), "AA");
+        mandatoryCourses.put(new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null), new Grade(new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null), 100));
         transcript.getTakenCouerses().putAll(mandatoryCourses);
         assertEquals(4.0, transcript.calculateGpa(), 0.0);
     }
-
-    @Test
-    public void testCalculateTakenCredit() {
-        HashMap<Course,String> mandatoryCourses = new HashMap<Course,String>();
-        mandatoryCourses.put(new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null), "AA");
-        mandatoryCourses.put(new MandatoryCourse("English", "ENG101", 3, 1, "08:00-09:00", 30, 1, null), "AA");
-        transcript.setTakenCouerses(mandatoryCourses);
-        assertEquals(6, transcript.calculateTakenCredit());
-    }
-
     @Test
     public void testHasBeenPassedCourse() {
         ArrayList<Course> mandatoryCourses = new ArrayList<Course>();
         MandatoryCourse mandatoryCourse1 = new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null);
         mandatoryCourses.add(mandatoryCourse1);
-        transcript.setCompletedCourses(mandatoryCourses);
+        transcript.getCompletedCourses().addAll(mandatoryCourses);
         assertEquals(true, transcript.hasBeenPassedCourse(mandatoryCourse1));
     }
 
@@ -48,9 +37,9 @@ public class TranscriptTest {
         MandatoryCourse mandatoryCourse1 = new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null);
         ArrayList<Course> mandatoryCourses = new ArrayList<Course>();
         mandatoryCourses.add(mandatoryCourse1);
-        transcript.setCompletedCourses(mandatoryCourses);
         ArrayList<Course> mandatoryCourses2 = new ArrayList<Course>();
         mandatoryCourses2.add(mandatoryCourse1);
+        transcript.getCompletedCourses().addAll(mandatoryCourses);
         assertEquals(true, transcript.hasBeenPassedCourses(mandatoryCourses2));
     }
 
