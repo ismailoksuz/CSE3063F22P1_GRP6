2721b6c936a7ad5caef83279dd7aa8f2a7a36fa2
emresagiroglu <94112475+emresagiroglu@users.noreply.github.com>
2022-12-17 12:12:52 +0300

    Update ScheduleTest.java

--- Iteration-2/src/ScheduleTest.java
+++ Iteration-2/src/ScheduleTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class ScheduleTest {
 
-    Schedule schedule = new Schedule(1, "08:00-09:00");
+    private Schedule schedule = new Schedule(1, "08:00-09:00");
     @Test
     public void testIsCollision() {
         assertEquals(schedule.isCollision(new Schedule(1, "08:00-09:00")), true);
