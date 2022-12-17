038dc6b502a224097028514df7dbcbe823dcb7d8
Erdem Pehlivanlar <85944761+erdemphl@users.noreply.github.com>
2022-12-17 13:07:15 +0300

    Update InstructorTest.java

--- Iteration-2/src/InstructorTest.java
+++ Iteration-2/src/InstructorTest.java
@@ -3,8 +3,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class InstructorTest {
-    Instructor instructor = new Instructor("Instructor", "Test");
-    MandatoryCourse course = new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null);
+    private Instructor instructor = new Instructor("Instructor", "Test");
+    private MandatoryCourse course = new MandatoryCourse("Math", "MATH101", 3, 1, "08:00-09:00", 30, 1, null);
 
     @Test
     public void testAddGivenCourse() {
