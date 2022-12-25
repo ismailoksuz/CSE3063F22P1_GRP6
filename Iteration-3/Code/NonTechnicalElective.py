1d1717a467b3f25277b68a84b01bd60455609d4c
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-12-25 16:31:55 +0300

    Update NonTechnicalElective.py

--- Iteration-3/Code/NonTechnicalElective.py
+++ Iteration-3/Code/NonTechnicalElective.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class NonTechnicalElective(ElectiveCourse):
     def __init__(self, course_name: str, course_code: str, course_credit: int, course_day: int,
                  course_hour: str, quota: int, semesters: List[int]):
