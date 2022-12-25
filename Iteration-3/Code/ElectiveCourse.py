1a1f6d9d200b6fdd8300407a924dd96abe06ba4b
emresagiroglu <94112475+emresagiroglu@users.noreply.github.com>
2022-12-25 17:07:15 +0300

    ElectiveCourse.py created

---
+++ Iteration-3/Code/ElectiveCourse.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class ElectiveCourse(Course):
+    def __init__(self, course_name: str, course_code: str, course_credit: int, course_day: int, course_hour: str,
+                 quato: int, semesters: List[int]):
+        super().__init__(course_name, course_code, course_credit, course_day, course_hour, quato)
+        self.semesters = semesters
+
+    def semester_control(self, student):
+        is_true = False
+        for semester in self.get_semesters():
+            if student.get_semester() == semester:
+                is_true = True
+                break
+        return is_true
+
+    def semester_check(self, semester: int) -> bool:
+        is_true = False
+        for s in self.get_semesters():
+            if s == semester:
+                is_true = True
+                break
+        return is_true
+
+    # GETTERS AND SETTERS
+    def get_semesters(self) -> List[int]:
+        return self.semesters
