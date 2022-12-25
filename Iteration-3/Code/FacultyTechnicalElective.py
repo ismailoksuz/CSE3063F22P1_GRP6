63792b55742503a5f86f53f3177bda2610ea18ae
mertkan-turan <96443582+mertkan-turan@users.noreply.github.com>
2022-12-25 20:46:06 +0300

    Add files via upload

---
+++ Iteration-3/Code/FacultyTechnicalElective.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from ElectiveCourse import ElectiveCourse
+
+
+class FacultyTechnicalElective(ElectiveCourse):
+    def __init__(self, courseName: str, courseCode: str, courseCredit: int, courseDay: int,
+                 courseHour: str, quato: int, semesters: List[int], prequisites: List[Course]):
+        super().__init__(courseName, courseCode, courseCredit, courseDay, courseHour, quato, semesters)
+        self.__prequisites = prequisites
+
+    def isEligibleToRequest(self, student: Student) -> bool:
+        return self.semesterControl(student) and student.getTranscript().hasBeenPassedCourses(self.getPrequisites) #look at here again
+
+    #define getter method
+    @property
+    def getPrequisites(self) -> List[Course]:
+        return self.__prequisites
+
+    # define setter method
+    def setPrequisites(self, prequisites: List[Course]):
+        self.__prequisites = prequisites
+
