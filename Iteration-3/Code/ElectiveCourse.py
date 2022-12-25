99264cc3827ee770d4da848b92b2663ffcd650ba
emresagiroglu <94112475+emresagiroglu@users.noreply.github.com>
2022-12-25 17:10:15 +0300

    Update ElectiveCourse.py

--- Iteration-3/Code/ElectiveCourse.py
+++ Iteration-3/Code/ElectiveCourse.py
@@ -4,7 +4,7 @@ class ElectiveCourse(Course):
     def __init__(self, course_name: str, course_code: str, course_credit: int, course_day: int, course_hour: str,
                  quato: int, semesters: List[int]):
         super().__init__(course_name, course_code, course_credit, course_day, course_hour, quato)
-        self.semesters = semesters
+        self.__semesters = semesters
 
     def semester_control(self, student):
         is_true = False
