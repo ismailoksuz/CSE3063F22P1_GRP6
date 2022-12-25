0265fa76479e8f28f309f3b56f868f988f309b0a
mertkan-turan <96443582+mertkan-turan@users.noreply.github.com>
2022-12-25 18:03:05 +0300

    Add files via upload

---
+++ Iteration-3/Code/Grade.py
@@ -0,0 +1,46 @@
+import random
+import logging #edit the log part
+
+
+class Grade:
+
+    def __init__(self, course: Course, courseGrade: int):
+        self.__course = course
+        self.__courseGrade = courseGrade
+        self.__letter = self.getLetterGrade()
+        logging.info("Grade created.")
+
+    def getLetterGrade(self):
+        letterGrade = ""
+        random_number = random.Random()
+        if self.__courseGrade > 100 or self.__courseGrade < 0:
+            exit(1)
+        elif self.__courseGrade >= 90:
+            letterGrade = "AA"
+        elif self.__courseGrade >= 85:
+            letterGrade = "BA"
+        elif self.__courseGrade >= 75:
+            letterGrade = "BB"
+        elif self.__courseGrade >= 65:
+            letterGrade = "CB"
+        elif self.__courseGrade >= 55:
+            letterGrade = "CC"
+        elif self.__courseGrade >= 45:
+            letterGrade = "DC"
+        elif self.__courseGrade >= 35:
+            letterGrade = "DD"
+        elif self.__courseGrade >= 30:
+            letterGrade = "FD"
+        else:
+            isAttend = (random_number.randint(0, 6) == 0)
+            isEnteredTheFinal = (random_number.randint(0, 6) == 0)
+            letterGrade = "FF" if isAttend and isEnteredTheFinal else "FG" if isAttend else "DZ"
+        return letterGrade
+
+    # GETTER AND SETTER
+    def getCourse(self) -> Course:
+        return self.__course
+
+    def getLetter(self) -> str:
+        return self.__letter
+
