5a82eb571bc8083f5d05056886c05ad68ef07603
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2022-12-26 14:36:10 +0300

    Update NonTechnicalElective.py

--- Iteration-3/Code/NonTechnicalElective.py
+++ Iteration-3/Code/NonTechnicalElective.py
@@ -1,9 +1,9 @@
 from typing import List
 
 class NonTechnicalElective(ElectiveCourse):
-    def __init__(self, course_name: str, course_code: str, course_credit: int, course_day: int,
-                 course_hour: str, quota: int, semesters: List[int]):
-        super().__init__(course_name, course_code, course_credit, course_day, course_hour, quota, semesters)
+    def __init__(self, courseName: str, courseCode: str, courseCredit: int, courseDay: int,
+                 courseHour: str, quota: int, semesters: List[int]):
+        super().__init__(courseName, courseCode, courseCredit, courseDay, courseHour, quota, semesters)
 
     def is_eligible_to_request(self, student: Student) -> bool:
         return self.semester_control(student)
