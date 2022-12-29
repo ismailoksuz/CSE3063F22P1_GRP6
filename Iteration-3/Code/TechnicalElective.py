5f1e5aa66d993522f0d74a3f9fe56e1b6c99f2cc
onuralkurt <54157952+onuralkurt@users.noreply.github.com>
2022-12-29 18:49:26 +0300

    Update TechnicalElective.py

--- Iteration-3/Code/TechnicalElective.py
+++ Iteration-3/Code/TechnicalElective.py
@@ -1,57 +1,18 @@
+import Course
+import ElectiveCourse
+import Student
 from typing import List
-import json
-from ElectiveCourse import ElectiveCourse
-from student import Student
-from course import Course
-
-class TechnicalElective(ElectiveCourse): # , ICreditRequirement will be added
-    allTechnicalElectives = []
+class TechnicalElective(ElectiveCourse): # ICreditRequirement will be added.****
     def __init__(self, courseName: str, courseCode: str, courseCredit: int, courseDay: int, courseHour: str, quota: int, semester: List[int], requiredCredits: int, prerequisites: List[Course]):
         super().__init__(courseName, courseCode, courseCredit, courseDay, courseHour, quota, semester)
         self.__requiredCredits = requiredCredits
         self.__prerequisites = prerequisites
 
-        TechnicalElective.allTechnicalElectives.append(self)
-
-
-
-    @classmethod
-    def read_from_json(cls):
-        try:
-            # Opening JSON file
-            input = open('input.json', encoding="utf-8")
-
-            # returns JSON objest as a dictionary
-            data = json.load(input)
-            requiredCredits = data["TERequiredCredits"]
-            for i in data['technicalElectiveCourses']:
-                TechnicalElective(
-                    courseName=i["courseName"],
-                    courseCode=i["courseCode"],
-                    quota=i["quota"],
-                    semester=i["semester"],
-                    courseCredit=i["credits"],
-                    courseDay=i["courseDay"],
-                    courseHour=i["courseHour"],
-                    requiredCredits= requiredCredits,
-                    prerequisites = i["preRequisites"]
-
-                )
-
-                # close file
-                input.close()
-        except IOError:
-            # Will be log
-            exit(1)
-
-
     def isEligibleToRequest(self, student: Student) -> bool:
         if self.semesterControl(student):
-            if not student.getTranscript().hasBeenPassedCourses(self.getPrequisites()):
+            if not student.getTranscript().hasBeenPassedCourses(self.getPrerequisites()):
                 self.setFailedPreq(self.getFailedPreq() + 1)
-                student.getStudentOutput().add(
-                    f"The system didn't allow {self.getCourseCode()} because student "
-                    f"failed prereq. {self.getPrequisites()[0].getCourseCode()}")
+                student.getStudentOutput().append(f"The system didn't allow {self.getCourseCode()} because student failed prereq. {self.getPrerequisites()[0].getCourseCode()}")
                 return False
             else:
                 return True
@@ -63,11 +24,8 @@ class TechnicalElective(ElectiveCourse): # , ICreditRequirement will be added
             return True
         else:
             self.setFailedCredits(self.getFailedCredits() + 1)
-            student.getStudentOutput.add(f"The advisor didn't approve TE{self.getCourseCode()} because student "
-                                         f"completed credits < {self.getRequiredCredits()}")
+            student.getStudentOutput().append(f"The advisor didn't approve TE{self.getCourseCode()} because student completed credits < {self.getRequiredCredits()}")
             return False
-
-
 
     def getRequiredCredits(self) -> int:
         return self.__requiredCredits
@@ -75,8 +33,8 @@ class TechnicalElective(ElectiveCourse): # , ICreditRequirement will be added
     def setRequiredCredits(self, requiredCredits: int):
         self.__requiredCredits = requiredCredits
 
-    def getPrequisites(self) -> List[Course]:
-        return self.__prequisites
+    def getPrerequisites(self) -> List[Course]:
+        return self.__prerequisites
 
-    def setPrequisites(self, prequisites: List[Course]):
-        self.__prequisites = prequisites
+    def setPrerequisites(self, prerequisites: List[Course]):
+        self.__prerequisites = prerequisites
