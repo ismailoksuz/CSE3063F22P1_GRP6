ef9160a06d42b35dbbf3edce11ef1b05b4187b3b
onuralkurt <54157952+onuralkurt@users.noreply.github.com>
2022-12-29 17:46:09 +0300

    Add files via upload

---
+++ Iteration-3/Code/Instructor.py
@@ -0,0 +1,20 @@
+import Person
+import Course
+from typing import List, cast
+
+class Instructor(Person):
+
+    def __init__(self, firstName: str, lastName: str, givenCourses: List[Course]=[]):
+        super().__init__(firstName, lastName)
+        self.__givenCourses = givenCourses
+        print(self.toString() + " named instructor created.") # will be log
+
+    def addGivenCourse(self, course):
+        self.__givenCourses.append(course)
+        print(self.toString() + ": instructor now gives " + course.getCourseName()) # will be log
+
+    def removeGivenCourse(self, course):
+        self.__givenCourses.remove(course)
+
+    def getGivenCourses(self):
+        return self.__givenCourses
