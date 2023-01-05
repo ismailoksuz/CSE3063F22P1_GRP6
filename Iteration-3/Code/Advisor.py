ae15bacd9d872fcea296114e9595d8472c5b58a6
Serkan Korkut <83539054+serkankorkut17@users.noreply.github.com>
2023-01-05 01:08:34 +0100

    Update Advisor.py

--- Iteration-3/Code/Advisor.py
+++ Iteration-3/Code/Advisor.py
@@ -1,6 +1,7 @@
 from Instructor import Instructor
 from Course import Course
 from typing import List
+import logging
 
 
 class Advisor(Instructor):
@@ -8,8 +9,8 @@ class Advisor(Instructor):
     def __init__(self, firstName: str, lastName: str, givenCourses: List[Course] = []):
         super().__init__(firstName, lastName, givenCourses)
         self.__students = []
-        print(self.getFirstName() + " " + self.getLastName() +
-              " named advisor created.")  # will be log
+        logging.info(self.getFirstName() + " " + self.getLastName() +
+                     " named advisor created.")  # will be log
 
     def completeRegistration(self, student):
         for i in range(len(student.getRequestedCourses())):
@@ -17,8 +18,8 @@ class Advisor(Instructor):
                 student.getTranscript().getEnrolledCourses().append(
                     student.getRequestedCourses()[i])
                 student.getRequestedCourses()[i].getStudents().append(student)
-        print(student.getStudentName() +
-              "'s registration successfully completed.")  # will be log
+        logging.info(student.getStudentName() +
+                     "'s registration successfully completed.")  # will be log
 
     def checkCollision(self, student, course):
         isTrue = True
@@ -27,8 +28,8 @@ class Advisor(Instructor):
                 course.setCollisionProblem(course.getCollisionProblem() + 1)
                 student.getStudentOutput().append("Advisor didn't approve " + course.getCourseCode() +
                                                   " because of two hours collision with " + c.getCourseCode() + " in schedule")
-                print(student.getStudentName() + " couldn't take " + course.getCourseName() +
-                      " because of collision with " + c.getCourseName() + ".")  # will be log
+                logging.info(student.getStudentName() + " couldn't take " + course.getCourseName() +
+                             " because of collision with " + c.getCourseName() + ".")  # will be log
                 isTrue = False
                 break
         return isTrue
@@ -40,8 +41,8 @@ class Advisor(Instructor):
             course.setQuotaProblem(course.getQuotaProblem() + 1)
             student.getStudentOutput().append("The student couldn't register for " +
                                               course.getCourseCode() + " because of a quota problem")
-            print(student.getStudentName() + " couldn't take " + course.getCourseName() +
-                  " because course quota is full ( Quota: " + str(course.getQuato()) + ").")  # will be log
+            logging.info(student.getStudentName() + " couldn't take " + course.getCourseName() +
+                         " because course quota is full ( Quota: " + str(course.getQuato()) + ").")  # will be log
             return False
 
     def checkEnrolledCourseLimit(self, course, student):
@@ -51,8 +52,8 @@ class Advisor(Instructor):
             course.setEnrolledCourseLimit(course.getEnrolledCourseLimit() + 1)
             student.getStudentOutput().append("The student couldn't register for " +
                                               course.getCourseCode() + " due to exceeding the maximum number of courses that can be registered")
-            print(student.getStudentName() + " couldn't take " + course.getCourseName() +
-                  " due to exceeding the maximum number of courses that can be registered.")  # will be log
+            logging.info(student.getStudentName() + " couldn't take " + course.getCourseName() +
+                         " due to exceeding the maximum number of courses that can be registered.")  # will be log
             return False
 
     def getStudents(self):
