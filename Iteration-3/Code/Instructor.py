2cacde52d4cc0e1f0f4ef02f5c0573594311ec95
Serkan Korkut <83539054+serkankorkut17@users.noreply.github.com>
2023-01-05 01:17:02 +0100

    Update Instructor.py

--- Iteration-3/Code/Instructor.py
+++ Iteration-3/Code/Instructor.py
@@ -1,17 +1,21 @@
+import logging
 from Person import Person
 from Course import Course
 from typing import List
 
+
 class Instructor(Person):
 
-    def __init__(self, firstName: str, lastName: str, givenCourses: List[Course]=[]):
+    def __init__(self, firstName: str, lastName: str, givenCourses: List[Course] = []):
         super().__init__(firstName, lastName)
         self.__givenCourses = givenCourses
-        print(self.toString() + " named instructor created.") # will be log
+        logging.info(self.toString() +
+                     " named instructor created.")  # will be log
 
     def addGivenCourse(self, course):
         self.__givenCourses.append(course)
-        print(self.toString() + ": instructor now gives " + course.getCourseName()) # will be log
+        logging.info(self.toString() + ": instructor now gives " +
+                     course.getCourseName())  # will be log
 
     def removeGivenCourse(self, course):
         self.__givenCourses.remove(course)
