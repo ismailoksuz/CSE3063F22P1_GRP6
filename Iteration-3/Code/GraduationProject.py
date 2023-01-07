7b5d2ad2344bf2bace889ff610c7a7f3a51f8157
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2023-01-07 16:42:45 +0300

    Update GraduationProject.py

--- Iteration-3/Code/GraduationProject.py
+++ Iteration-3/Code/GraduationProject.py
@@ -1,25 +1,32 @@
-#  import logging
-from typing import List
+import logging
+from ICreditRequirement import ICreditRequirement
+from MandatoryCourse import MandatoryCourse
+
 
 class GraduationProject(MandatoryCourse, ICreditRequirement):
+
     def __init__(self, courseName, courseCode, courseCredit, courseDay, courseHour,
-                 quota, semester, prequisites, requiredCredits):
-        super().__init__(courseName, courseCode, courseCredit, courseDay, courseHour, quota, semester, prequisites)
-        self.requiredCredits = requiredCredits
-      #  logging.info(f"{self.courseName} ({self.courseCode}) named graduation project created.")
+                 quota, semester, prerequisites, requiredCredits):
+        super().__init__(courseName, courseCode, courseCredit,
+                         courseDay, courseHour, quota, semester, prerequisites)
+        self.__requiredCredits = requiredCredits
+        logging.info(
+            f"{self.getCourseName()} ({self.getCourseCode()}) named graduation project created.")
 
     def isEligibleToRequest(self, student):
         return super().isEligibleToRequest(student) and self.checkRequiredCredit(student)
 
     def checkRequiredCredit(self, student):
-        if student.transcript.CreditCompleted >= self.requiredCredits:
+        if student.getTranscript().getCreditCompleted() >= self.__requiredCredits:
             return True
         else:
-            self.FailedCredits += 1
-            student.StudentOutput.append(f"The advisor didn't approve graduation project {self.courseCode} because student completed credits < {self.requiredCredits}")
-          #  logging.info(f"Student has not enough credits. {student.studentName} cannot enroll {self.courseName}.")
+            newFailedCredit = self.getFailedCredits() + 1
+            self.setFailedCredits(newFailedCredit)
+            student.getStudentOutput().append(
+                f"The advisor didn't approve graduation project {self.getCourseCode()} because student completed credits < {self.__requiredCredits}")
+            logging.info(
+                f"Student has not enough credits. {student.getStudentName()} cannot enroll {self.getCourseName()}.")
             return False
 
-    @property
-    def requiredCredits(self):
-        return self.requiredCredits
+    def getRequiredCredits(self):
+        return self.__requiredCredits
