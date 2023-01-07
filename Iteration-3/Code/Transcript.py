86382af2d96d55bdc7ca1aabe4f5c5a6f6498f64
onuralkurt <54157952+onuralkurt@users.noreply.github.com>
2023-01-07 16:45:22 +0300

    Add files via upload

--- Iteration-3/Code/Transcript.py
+++ Iteration-3/Code/Transcript.py
@@ -1,22 +1,24 @@
-#This will be the code from Transcript.java translated from java into python
-
-import logging
+from Course import Course
+from typing import List, cast
 
 
 class Transcript:
-    def __init__(self, student):
-        self.__log = logging.getLogger(Transcript.__name__)
-        self.__gpa = 0.0
+
+    def __init__(self):
+        self.__gpa = 0
         self.__creditCompleted = 0
-        self.__takenCourses = {}
-        self.__completedCourses = []
-        self.__failedCourses = []
-        self.__enrolledCourses = []
+        self.__takenCourses = dict()
+        self.__completedCourses = list()
+        self.__failedCourses = list()
+        self.__enrolledCourses = list()
 
     def calculateGpa(self):
         gradeMultiplication = 0
         totalCredit = 0
         studentGpa = 0
+
+        if len(self.__takenCourses) == 0:
+            return 0
 
         for course, grade in self.__takenCourses.items():
             totalCredit += course.getCourseCredit()
@@ -36,13 +38,11 @@ class Transcript:
                 gradeMultiplication += course.getCourseCredit() * 1.0
             elif grade.getLetter() == "FD":
                 gradeMultiplication += course.getCourseCredit() * 0.5
-            '''elif grade.getLetter() == "FF":
+            elif grade.getLetter() == "FF":
                 gradeMultiplication += course.getCourseCredit() * 0.0
             elif grade.getLetter() == "FG":
                 gradeMultiplication += course.getCourseCredit() * 0.0
             elif grade.getLetter() == "DZ":
-                gradeMultiplication += course.getCourseCredit() * 0.0'''
-            elif grade.getLetter() in ["FF","FG","DZ"]:
                 gradeMultiplication += course.getCourseCredit() * 0.0
         studentGpa = round(gradeMultiplication / totalCredit * 100.0) / 100.0
         self.__gpa = studentGpa
@@ -54,9 +54,9 @@ class Transcript:
             completedCredit += course.getCourseCredit()
         self.__creditCompleted = completedCredit
         return completedCredit
-        
+
     def isCourseCompletedOrFailed(self, course, letter):
-        if letter == "AA" or letter == "BA" or letter == "BB" or letter == "CB" or letter == "CC" or letter == "DC" or letter == "DD":
+        if letter == "AA" or letter == "BA" or letter == "BB" or letter == "CB" or letter == "CC" or letter == "DC" or letter == "DD" or letter == "S":
             self.__completedCourses.append(course)
         else:
             self.__failedCourses.append(course)
@@ -64,43 +64,37 @@ class Transcript:
     def hasBeenPassedCourse(self, course):
         if course == None:
             return True
-        return course in self.__completedCourses
+        if course in self.getCompletedCourses():
+            return True
+        else:
+            return False
 
-    def hasBeenPassedCourses(self, courses):
+    def hasBeenPassedCourses(self, courses):  # infinite loop
         for course in courses:
             if not self.hasBeenPassedCourse(course):
                 return False
         return True
 
-    @property
-    def gpa(self):
+    def getGpa(self):
         return self.__gpa
 
-    @gpa.setter
-    def gpa(self, gpa):
+    def setGpa(self, gpa):
         self.__gpa = gpa
-    
-    @property
-    def creditCompleted(self):
-        return self.__creditCompleted   
-    
-    @creditCompleted.setter
-    def creditCompleted(self, creditCompleted):
+
+    def getCreditCompleted(self):
+        return self.__creditCompleted
+
+    def setCreditCompleted(self, creditCompleted):
         self.__creditCompleted = creditCompleted
-    
-    @property
-    def takenCourses(self):
+
+    def getTakenCourses(self):
         return self.__takenCourses
-    
-    @property
-    def completedCourses(self):
+
+    def getCompletedCourses(self):
         return self.__completedCourses
-    
-    @property
-    def failedCourses(self):
+
+    def getFailedCourses(self):
         return self.__failedCourses
-    
-    @property
-    def enrolledCourses(self):
+
+    def getEnrolledCourses(self):
         return self.__enrolledCourses
-        
