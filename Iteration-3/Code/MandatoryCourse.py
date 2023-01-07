32b4474cd30726416c242453c73687d7dcfb4b08
İsmail Öksüz <46068969+ismailoksuz@users.noreply.github.com>
2023-01-07 16:43:17 +0300

    Update MandatoryCourse.py

--- Iteration-3/Code/MandatoryCourse.py
+++ Iteration-3/Code/MandatoryCourse.py
@@ -1,5 +1,6 @@
-
+from Course import Course
 from typing import List
+
 
 class MandatoryCourse(Course):
     def __init__(self, courseName: str, courseCode: str, courseCredit: int, courseDay: int, courseHour: str,
@@ -7,13 +8,13 @@ class MandatoryCourse(Course):
         super().__init__(courseName, courseCode, courseCredit, courseDay, courseHour, quota)
         self.__semester = semester
         self.__prerequisites = prerequisites
-    
+
     def isEligibleToRequest(self, student):
         if (student.getSemester() == self.getSemester()):
-            if (not student.getTranscript().hasBeenPassedCourses(self.getPrequisites())):
+            if (not student.getTranscript().hasBeenPassedCourses(self.getPrerequisites())):
                 self.setFailedPreq(self.getFailedPreq() + 1)
-                student.getStudentOutput().add("The system didn't allow " + self.getCourseCode() + 
-                "because student failed prereq." + self.getPrequisites()[0].getCourseCode())
+                # student.getStudentOutput().append("The system didn't allow " + self.getCourseCode() + "because student failed prereq." + self.getPrequisites()[0].getCourseCode())
+                student.getStudentOutput().append("The system didn't allow " + self.getCourseCode() + " because student failed prereq.")
                 return False
             else:
                 return True
@@ -21,11 +22,18 @@ class MandatoryCourse(Course):
             return False
 
     def isEligibleToBePreviouslyTaken(self, student):
-        return student.getSemester() > self.getSemester() and student.getTranscript().hasBeenPassedCourses(self.getPrequisites())
+        if student.getSemester() > self.getSemester():
+            if student.getTranscript().hasBeenPassedCourses(self.getPrerequisites()):
+                return True
+            else:
+                return False
+        else:
+            return False
+        # return (student.getSemester() > self.getSemester()) and (student.getTranscript().hasBeenPassedCourses(self.getPrerequisites()))
 
     # GETTERS AND SETTERS
     def getSemester(self) -> int:
         return self.__semester
 
-    def getPrequisites(self) -> List[Course]:
+    def getPrerequisites(self) -> List[Course]:
         return self.__prerequisites
