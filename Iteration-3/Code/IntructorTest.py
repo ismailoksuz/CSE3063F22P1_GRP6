746892bdb6f0c90fbc3c39415a90687e3e122fb2
Mustafa Yanar <mustafayanar0007@gmail.com>
2023-01-08 17:53:25 +0300

    __eq__ overriding in Course class

--- Iteration-3/Code/IntructorTest.py
+++ Iteration-3/Code/IntructorTest.py
@@ -11,7 +11,6 @@ class InstructorTest(unittest.TestCase):
 
         self.instructor = Instructor(firstName, lastName)
         self.course = MandatoryCourse("Introduction to Computer Science", "CSE1013", 4, 1, "9:00AM", 30, 1, [])
-        self.course2 = MandatoryCourse("Introduction to Computer Graphics", "CSE4067", 4, 1, "11:00AM", 50, 1, [])
 
     def test_addGivenCourse(self):
         self.assertEqual(len(self.instructor.getGivenCourses()), 0)
@@ -19,10 +18,8 @@ class InstructorTest(unittest.TestCase):
         self.assertEqual(len(self.instructor.getGivenCourses()), 1)
 
     def test_removeGivenCourse(self):
-        self.instructor.addGivenCourse(self.course2)
-        self.assertEqual(len(self.instructor.getGivenCourses()), 2)
-        self.instructor.removeGivenCourse(self.course2)
-        self.assertEqual(len(self.instructor.getGivenCourses()), 1)
+        self.instructor.removeGivenCourse(self.course)
+        self.assertEqual(len(self.instructor.getGivenCourses()), 0)
 
 if __name__ == '__main__':
     unittest.main()
